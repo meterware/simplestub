689a19845e72e9a7af26dd6b899dbe3ad5b22889
Russell Gold <russell.gold@oracle.com>
2013-04-17 13:21:02 -0400

    Add basic documentation to the web site

--- src/main/java/org/glassfish/simplestub/Stub.java
+++ src/main/java/org/glassfish/simplestub/Stub.java
@@ -2,7 +2,6 @@ package org.glassfish.simplestub;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
