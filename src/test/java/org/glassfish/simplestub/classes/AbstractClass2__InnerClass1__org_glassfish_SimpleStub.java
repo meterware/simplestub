b7441ad83e8fbe4efd6b84ddd8c6914e7301b100
Russell Gold <russ@gold-family.us>
2014-04-01 00:38:13 -0400

    remove old org.glassfish classes

--- src/test/java/org/glassfish/simplestub/classes/AbstractClass2__InnerClass1__org_glassfish_SimpleStub.java
+++
@@ -1,8 +0,0 @@
-package org.glassfish.simplestub.classes;
-
-public class AbstractClass2__InnerClass1__org_glassfish_SimpleStub extends AbstractClass2.InnerClass1 {
-
-    @Override
-    public void method1() {
-    }
-}
--- src/test/java/org/glassfish/simplestub/classes/Interface1.java
+++
@@ -1,6 +0,0 @@
-package org.glassfish.simplestub.classes;
-
-public interface Interface1 {
-
-    void method1();
-}
