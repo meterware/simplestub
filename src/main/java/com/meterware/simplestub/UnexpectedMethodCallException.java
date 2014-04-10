8fd92dac0ac9f875d872c88ef440906bd9852d18
Russell Gold <russ@gold-family.us>
2014-04-09 22:40:59 -0400

    Enable Javadoc report

--- src/main/java/com/meterware/simplestub/UnexpectedMethodCallException.java
+++ src/main/java/com/meterware/simplestub/UnexpectedMethodCallException.java
@@ -1,5 +1,8 @@
 package com.meterware.simplestub;
 
+/**
+ * Thrown by a strict stub if one of its undefined methods is called.
+ */
 public class UnexpectedMethodCallException extends SimpleStubException {
     public UnexpectedMethodCallException(String message) {
         super(message);
