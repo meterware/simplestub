7b7b92b4b9d891314e4856633b33f732c21eb69c
Russell Gold <russ@gold-family.us>
2015-05-31 18:30:01 -0400

    add support for system properties

---
+++ src/main/java/com/meterware/simplestub/SystemPropertySupport.java
@@ -0,0 +1,50 @@
+package com.meterware.simplestub;
+
+/**
+ * A class which simplifies setting system properties in unit tests.
+ */
+public class SystemPropertySupport {
+
+    /**
+     * Sets a property to the specified value, returning a memento to restore it.
+     * @param propertyName the name of the property to set
+     * @param newValue the new value for the property
+     */
+    public static Memento install(String propertyName, String newValue) {
+        PropertyMemento memento = new PropertyMemento(propertyName);
+        System.setProperty(propertyName, newValue);
+        return memento;
+    }
+
+    /**
+     * Returns a memento to restore the specified property to its current value.
+     * @param propertyName the name of the property to preserve
+     */
+    public static Memento preserve(String propertyName) {
+        return new PropertyMemento(propertyName);
+    }
+
+    private static class PropertyMemento implements Memento {
+        private final String originalValue;
+        private String propertyName;
+
+        public PropertyMemento(String propertyName) {
+            this.propertyName = propertyName;
+            this.originalValue = System.getProperty(propertyName);
+        }
+
+        @Override
+        public void revert() {
+            if (originalValue == null)
+                System.clearProperty(propertyName);
+            else
+                System.setProperty(propertyName, originalValue);
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public String getOriginalValue() {
+            return originalValue;
+        }
+    }
+}
---
+++ src/test/java/com/meterware/simplestub/SystemPropertySupportTest.java
@@ -0,0 +1,93 @@
+package com.meterware.simplestub;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+/**
+ * Tests behavior for setting and restoring system properties.
+ */
+public class SystemPropertySupportTest {
+
+    /** The name of a property originally set. **/
+    private static final String PROPERTY_1 = "FirstProperty";
+
+    /** The name of a property originally clear. **/
+    private static final String PROPERTY_2 = "SecondProperty";
+
+    /** The original value of property 1. */
+    private static final String ORIGINAL_VALUE = "original value";
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty(PROPERTY_1, ORIGINAL_VALUE);
+        System.clearProperty(PROPERTY_2);
+    }
+
+    @Test
+    public void whenInstalled_systemPropertyIsChanged() throws Exception {
+        SystemPropertySupport.install(PROPERTY_1, "alternate value");
+
+        assertThat(System.getProperty(PROPERTY_1), is("alternate value"));
+    }
+
+    @Test
+    public void afterPropertyInstalled_retrieveOriginalValue() throws Exception {
+        Memento memento = SystemPropertySupport.install(PROPERTY_1, "alternate value");
+
+        assertThat((String) memento.getOriginalValue(), is(ORIGINAL_VALUE));
+    }
+
+    @Test
+    public void whenMementoRevertedAfterInstall_originalSystemPropertyValueIsRestored() throws Exception {
+        Memento memento = SystemPropertySupport.install(PROPERTY_1, "alternate value");
+
+        memento.revert();
+
+        assertThat(System.getProperty(PROPERTY_1), is(ORIGINAL_VALUE));
+    }
+
+    @Test
+    public void whenPreserved_systemPropertyIsUnchanged() throws Exception {
+        SystemPropertySupport.preserve(PROPERTY_1);
+
+        assertThat(System.getProperty(PROPERTY_1), is(ORIGINAL_VALUE));
+    }
+
+    @Test
+    public void afterPropertyPreservedAndSet_retrieveOriginalValue() throws Exception {
+        Memento memento = SystemPropertySupport.preserve(PROPERTY_1);
+        System.setProperty(PROPERTY_1, "random");
+
+        assertThat((String) memento.getOriginalValue(), is(ORIGINAL_VALUE));
+    }
+
+    @Test
+    public void whenMementoRevertedAfterPreserve_originalSystemPropertyValueIsRestored() throws Exception {
+        Memento memento = SystemPropertySupport.preserve(PROPERTY_1);
+        System.setProperty(PROPERTY_1, "random");
+
+        memento.revert();
+
+        assertThat(System.getProperty(PROPERTY_1), is(ORIGINAL_VALUE));
+    }
+
+    @Test
+    public void whenPropertyOriginallyNotSet_originalValueIsNull() throws Exception {
+        Memento memento = SystemPropertySupport.install(PROPERTY_2, "alternate value");
+
+        assertThat(memento.getOriginalValue(), nullValue());
+    }
+
+    @Test
+    public void whenPropertyOriginallyNotSet_revertRestoresToNull() throws Exception {
+        Memento memento = SystemPropertySupport.install(PROPERTY_2, "alternate value");
+
+        memento.revert();
+
+        assertThat(System.getProperty(PROPERTY_2), nullValue());
+    }
+}
