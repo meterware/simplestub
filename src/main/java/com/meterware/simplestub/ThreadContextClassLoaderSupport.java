b5ccc8f30f7fd549b6821d4258260e946e00fbaa
Russell Gold <russ@gold-family.us>
2015-05-31 18:28:50 -0400

    Add support for class loaders

---
+++ src/main/java/com/meterware/simplestub/ThreadContextClassLoaderSupport.java
@@ -0,0 +1,55 @@
+package com.meterware.simplestub;
+
+/**
+ * Support for manipulating the thread context class-loader in a unit test.
+ */
+public class ThreadContextClassLoaderSupport {
+
+    /**
+     * Installs a classloader as the current thread context classloader, returning a memento which can be used to
+     * undo the installation.
+     * @param classLoader the classloader to install.
+     */
+    public static Memento install(ClassLoader classLoader) {
+        Memento memento = new ThreadContextClassLoaderMemento();
+        Thread.currentThread().setContextClassLoader(classLoader);
+        return memento;
+    }
+
+    /**
+     * Returns a memento to restore the current thread context classloader.
+     */
+    public static Memento preserve() {
+        return new ThreadContextClassLoaderMemento();
+    }
+
+    /**
+     * Creates a test implementation or subclass of a given class with a specified name in the current thread context
+     * class-loader. Note that the base class must be accessible from the context class-loader, and if not an interface,
+     * must have a public no-arg constructor.
+     * @param className the name to use for the created class.
+     * @param aClass a base class or interface to use as a parent for the specified class.
+     */
+    public static void createStubInThreadContextClassLoader(String className, Class<?> aClass) {
+        new StubLoader(aClass, false).getStubClassForThread(className);
+    }
+
+    static private class ThreadContextClassLoaderMemento implements Memento {
+        private ClassLoader originalValue;
+
+        public ThreadContextClassLoaderMemento() {
+            originalValue = Thread.currentThread().getContextClassLoader();
+        }
+
+        @Override
+        public void revert() {
+            Thread.currentThread().setContextClassLoader(originalValue);
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public ClassLoader getOriginalValue() {
+            return originalValue;
+        }
+    }
+}
---
+++ src/test/java/com/meterware/simplestub/ThreadContextClassLoaderSupportTest.java
@@ -0,0 +1,137 @@
+package com.meterware.simplestub;
+
+import com.meterware.simplestub.classes.ClassWithConstructorParameters;
+import com.meterware.simplestub.classes.ConcreteClass;
+import com.meterware.simplestub.classes.Interface1;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.EventListener;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.hamcrest.Matchers.typeCompatibleWith;
+
+/**
+ * Tests support for context class loaders.
+ */
+public class ThreadContextClassLoaderSupportTest {
+    private static final String CREATED_CLASS_NAME = "a.b.C";
+    private ClassLoader savedClassLoader;
+
+    @Before
+    public void setUp() throws Exception {
+        savedClassLoader = Thread.currentThread().getContextClassLoader();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.currentThread().setContextClassLoader(savedClassLoader);
+    }
+
+    @Test
+    public void whenInstalled_classLoaderIsSetForThread() throws Exception {
+        ClassLoader classLoader = new URLClassLoader(new URL[0]);
+        ThreadContextClassLoaderSupport.install(classLoader);
+
+        assertThat(Thread.currentThread().getContextClassLoader(), sameInstance(classLoader));
+    }
+
+    @Test
+    public void afterInstalled_retrieveOriginalValue() throws Exception {
+        ClassLoader classLoader = new URLClassLoader(new URL[0]);
+        Memento memento = ThreadContextClassLoaderSupport.install(classLoader);
+
+        assertThat((ClassLoader) memento.getOriginalValue(), sameInstance(savedClassLoader));
+    }
+
+    @Test
+    public void whenMementoReverted_originalValueIsRestored() throws Exception {
+        ClassLoader classLoader = new URLClassLoader(new URL[0]);
+        Memento memento = ThreadContextClassLoaderSupport.install(classLoader);
+
+        memento.revert();
+
+        assertThat(Thread.currentThread().getContextClassLoader(), sameInstance(savedClassLoader));
+    }
+
+    @Test
+    public void whenPreserved_originalValueIsUnchanged() throws Exception {
+        ThreadContextClassLoaderSupport.preserve();
+
+        assertThat(Thread.currentThread().getContextClassLoader(), sameInstance(savedClassLoader));
+    }
+
+    @Test
+    public void afterPreservedAndSet_retrieveOriginalClassLoader() throws Exception {
+        Memento memento = ThreadContextClassLoaderSupport.preserve();
+        Thread.currentThread().setContextClassLoader(new URLClassLoader(new URL[0]));
+
+        assertThat((ClassLoader) memento.getOriginalValue(), sameInstance(savedClassLoader));
+    }
+
+    @Test
+    public void whenMementoRevertedAfterPreserve_originalClassLoaderIsRestored() throws Exception {
+        Memento memento = ThreadContextClassLoaderSupport.preserve();
+        Thread.currentThread().setContextClassLoader(new URLClassLoader(new URL[0]));
+
+        memento.revert();
+        assertThat(Thread.currentThread().getContextClassLoader(), sameInstance(savedClassLoader));
+    }
+
+    @Test
+    public void whenClassDefinedFromInterface_retrieveFromClassLoader() throws Exception {
+        ClassLoader classLoader = createStubInThreadContextClassLoader(Interface1.class);
+
+        assertThat(classLoader.loadClass(CREATED_CLASS_NAME), typeCompatibleWith(Interface1.class));
+    }
+
+    private ClassLoader createStubInThreadContextClassLoader(Class<?> baseClass) {
+        ClassLoader classLoader = new URLClassLoader(new URL[0], baseClass.getClassLoader());
+        ThreadContextClassLoaderSupport.install(classLoader);
+        ThreadContextClassLoaderSupport.createStubInThreadContextClassLoader(CREATED_CLASS_NAME, baseClass);
+        return classLoader;
+    }
+
+    @Test
+    public void whenClassDefinedFromInterface_instantiateWithNoArgConstructor() throws Exception {
+        ClassLoader classLoader = createStubInThreadContextClassLoader(Interface1.class);
+
+        assertThat(classLoader.loadClass(CREATED_CLASS_NAME).newInstance(), instanceOf(Interface1.class));
+    }
+
+    @Test
+    public void whenClassDefinedFromJDKInterface_retrieveFromClassLoader() throws Exception {
+        ClassLoader classLoader = new URLClassLoader(new URL[0]);
+        ThreadContextClassLoaderSupport.install(classLoader);
+        ThreadContextClassLoaderSupport.createStubInThreadContextClassLoader(CREATED_CLASS_NAME, EventListener.class);
+
+        assertThat(classLoader.loadClass(CREATED_CLASS_NAME), typeCompatibleWith(EventListener.class));
+    }
+
+    @Test
+    public void whenClassDefinedFromTestClass_retrieveFromClassLoader() throws Exception {
+        ClassLoader classLoader = createStubInThreadContextClassLoader(ConcreteClass.class);
+
+        assertThat(classLoader.loadClass(CREATED_CLASS_NAME), typeCompatibleWith(ConcreteClass.class));
+    }
+
+    @Test(expected = SimpleStubException.class)
+    public void whenBaseClassLacksNoArgContructor_throwException() throws Exception {
+        createStubInThreadContextClassLoader(ClassWithConstructorParameters.class);
+    }
+
+    @Test(expected = SimpleStubException.class)
+    public void whenBaseClassNotAvailableFromCCL_throwException() throws Exception {
+        ClassLoader classLoader = createStubInThreadContextClassLoader(ConcreteClass.class);
+
+        ThreadContextClassLoaderSupport.install(new URLClassLoader(new URL[0]));
+        ThreadContextClassLoaderSupport.createStubInThreadContextClassLoader("new.Subclass", classLoader.loadClass(CREATED_CLASS_NAME));
+    }
+
+    // todo non-public base class in different package
+}
