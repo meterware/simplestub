0cc5bcf69eb5aa21626ed3e02c487eae5b989d69
Russell Gold <russ@gold-family.us>
2017-08-04 13:29:18 -0400

    Don't run javassist tests with JDK9

--- src/test/java/com/meterware/simplestub/generation/javassist/JavassistStubGeneratorTest.java
+++ src/test/java/com/meterware/simplestub/generation/javassist/JavassistStubGeneratorTest.java
@@ -1,6 +1,8 @@
 package com.meterware.simplestub.generation.javassist;
 
 import com.meterware.simplestub.generation.StubGeneratorTestBase;
+import org.junit.Assume;
+import org.junit.BeforeClass;
 
 /**
  * Tests for creating stubs using Javassist.
@@ -11,6 +13,18 @@ public class JavassistStubGeneratorTest extends StubGeneratorTestBase {
     public JavassistStubGeneratorTest() {
         super(STUB_GENERATOR_FACTORY);
     }
+
+
+    @BeforeClass
+    public static void skipUnderJdk9() throws Exception {
+        Assume.assumeTrue( isBeforeJdk9() );
+    }
+
+
+    private static boolean isBeforeJdk9() {
+        return System.getProperty( "java.version" ).startsWith( "1." );
+    }
+
 
     @Override
     protected String getImplementationType() {
